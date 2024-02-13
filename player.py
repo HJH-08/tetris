f549c000c6c9f3975e6f5eec2dc12225a19e7f27
HJH-08 <zcabjhh@ucl.ac.uk>
2024-02-13 17:13:43 +0000

    Update player.py

--- player.py
+++ player.py
@@ -2,10 +2,9 @@ from board import Direction, Rotation, Action
 from random import Random
 import time
 
-
+previous_discard = False
+number_of_discards = 10
 number_of_bombs = 5
-two_block_score = 9999
-next_actions_list = []
 rotations_list = [[],[Rotation.Clockwise],[Rotation.Clockwise,Rotation.Clockwise], [Rotation.Anticlockwise]]
 actions_list = [[Direction.Left], [Direction.Left, Direction.Left], [Direction.Left, Direction.Left, Direction.Left], [Direction.Left, Direction.Left, 
 Direction.Left, Direction.Left], [Direction.Left, Direction.Left, Direction.Left, Direction.Left, Direction.Left], [Direction.Right], [Direction.Right, Direction.Right], 
@@ -24,136 +23,154 @@ class Player:
                 else:
                     s += "."
             print(s, y)
-
-    def move_to_target(self, cloned_board, target_x):
-        has_landed = False
-        while target_x<cloned_board.falling.left and has_landed == False:
-            has_landed = cloned_board.move(Direction.Left)    
-        while target_x>cloned_board.falling.right and has_landed == False:
-            has_landed = cloned_board.move(Direction.Right)
-        return has_landed  
     
     def score(self, cloned_board):
-
         list_of_cells = cloned_board.cells
         sorted_list_of_cells = sorted(list(list_of_cells), key=lambda x: (x[0], x[1]))
-
         number_of_holes = 0
         for (x,y) in list_of_cells:
             if y==23:
                 continue
             else:
-                while y<=23:
+                while y<23:
                     if (x,y+1) in list_of_cells:
                         break
                     number_of_holes+=1
                     y+=1
         sum_of_heights = 0
-        consecutive_height_difference = 0
+        bumpiness = 0
         highest_y_point = 24
         highest_cell_in_each_column = {0:24, 1:24, 2:24, 3: 24, 4: 24, 5: 24, 6: 24, 7: 24, 8: 24, 9: 24}
         for (x,y) in sorted_list_of_cells:
             if highest_cell_in_each_column[x] >= y:
                 highest_cell_in_each_column[x] = y
         for column in range(0,9):
-            consecutive_height_difference += (abs(highest_cell_in_each_column[column] - highest_cell_in_each_column[column+1]))
+            bumpiness += ((abs(highest_cell_in_each_column[column] - highest_cell_in_each_column[column+1]))**2)
+
+
+
+        
         highest_y_point = min(highest_cell_in_each_column.values())
         lowest_y_point = max(highest_cell_in_each_column.values())
         greatest_height_difference = lowest_y_point - highest_y_point
         for height in highest_cell_in_each_column.values():
             sum_of_heights += (24-height)
-        
-        weightage_1 = 0.7
-        weightage_2 = 2.5
-        weightage_3 = 0.3
-        weightage_4 = 0.8
-        return weightage_1*(greatest_height_difference) + weightage_2*(number_of_holes) + weightage_3*(consecutive_height_difference) + weightage_4*sum_of_heights
+
+        if highest_y_point<9:
+            greatest_height_difference += 50
+        if sum_of_heights>100 or highest_y_point<12:
+            weightage_1 = 1.5
+            weightage_2 = 150
+            weightage_3 = 8
+            weightage_4 = 30
+        else:
+            weightage_1 = 1.2
+            weightage_2 = 150
+            weightage_3 = 1.5
+            weightage_4 = 10
+
+        return weightage_1*(greatest_height_difference) + weightage_2*(number_of_holes) + weightage_3*(bumpiness) + weightage_4*sum_of_heights
     
     def test_action(self, board):
 
-        global number_of_bombs
-        global next_actions_list
-        global two_block_score
-
-
-        scores = []
+        global number_of_discards
+        global previous_discard
         one_block_scores = []
-        # sum_of_heights = 0
-        # highest_cell_in_each_column = {0:24, 1:24, 2:24, 3: 24, 4: 24, 5: 24, 6: 24, 7: 24, 8: 24, 9: 24}
-        # for (x,y) in board.cells:
-        #     if highest_cell_in_each_column[x] >= y:
-        #         highest_cell_in_each_column[x] = y
-        # for height in highest_cell_in_each_column.values():
-        #     sum_of_heights += (24-height)
 
         for rotations in rotations_list:
             for actions in actions_list:
                 cloned_board = board.clone()
+                previous_number_of_cells = len(cloned_board.cells)
                 has_landed = False
                 for rotation in rotations:
-                    cloned_board.rotate(rotation)
+                    if not has_landed:
+                        has_landed = cloned_board.rotate(rotation)
                 for action in actions:
                     if not has_landed:
                         has_landed = cloned_board.move(action)
                 if not has_landed:
                     has_landed = cloned_board.move(Direction.Drop)
                 score = self.score(cloned_board)
-                one_block_scores.append(score)
-        # for rotations in rotations_list:
-        #     for actions in actions_list:
-        #         cloned_board = board.clone()
-        #         has_landed = False
-        #         for rotation in rotations:
-        #             cloned_board.rotate(rotation)
-        #         for action in actions:
-        #             if not has_landed:
-        #                 has_landed = cloned_board.move(action)
-        #         if not has_landed:
-        #             has_landed = cloned_board.move(Direction.Drop)
-        #         for rotations in rotations_list:
-        #             for actions in actions_list:
-        #                 cloned_twice_board = cloned_board.clone()
-        #                 has_landed = False
-        #                 for rotation in rotations:
-        #                     cloned_twice_board.rotate(rotation)
-        #                 for action in actions:
-        #                     if not has_landed:
-        #                         has_landed = cloned_twice_board.move(action)
-        #                 if not has_landed:
-        #                     has_landed = cloned_twice_board.move(Direction.Drop)
-        #                 score = self.score(cloned_twice_board)
-        #                 scores.append(score)
-        # if min(one_block_scores)>two_block_score:
-        #     current_actions_list = next_actions_list
-        #     next_actions_list = []
-        #     print("ENTIRE TWO BLOCK SEQUENCE ACTIVE", min(one_block_scores), two_block_score)
-        #     two_block_score = 9999
 
-        # elif min(one_block_scores)<min(scores):
+                sum_of_heights = 0
+                highest_cell_in_each_column = {0:24, 1:24, 2:24, 3: 24, 4: 24, 5: 24, 6: 24, 7: 24, 8: 24, 9: 24}
+                for (x,y) in board.cells:
+                    if highest_cell_in_each_column[x] >= y:
+                        highest_cell_in_each_column[x] = y
+                for height in highest_cell_in_each_column.values():
+                    sum_of_heights += (24-height)
+
+                after_number_of_cells = len(cloned_board.cells)
+                
+                highest_y_point = min(highest_cell_in_each_column.values())
+           
+                if sum_of_heights>100 or highest_y_point<12:
+                    score = score
+                else:
+                    if after_number_of_cells-previous_number_of_cells == -6:
+                        score = score * 1.33
+                    elif after_number_of_cells-previous_number_of_cells == -16:
+                        score = score * 1.12
+                    elif after_number_of_cells-previous_number_of_cells == -26:
+                        score = score * 0.7
+                    elif after_number_of_cells-previous_number_of_cells == -36:
+                        score = score * 0.01
+                one_block_scores.append(score)
         index = one_block_scores.index(min(one_block_scores))
+
         current_actions_list = rotations_list[int(index/len(actions_list))] + actions_list[int(index%len(actions_list))] + [Direction.Drop]
-            # two_block_score = 9999
-            # print("ONE BLOCK SEQUENCE")
-        # else:
-        #     index = scores.index(min(scores))
-        #     two_block_score = min(scores)
-        #     current_actions_list = rotations_list[int(index/(len(actions_list) * len(actions_list) * len(rotations_list)))] + actions_list[int(index%(len(actions_list) * len(actions_list) * len(rotations_list))/(len(actions_list) * len(rotations_list)))]+ [Direction.Drop]
-        #     if not next_actions_list:
-        #         next_actions_list = rotations_list[int((index%(len(actions_list)*len(rotations_list)))/len(actions_list))] + actions_list[int(index%(len(actions_list)*len(rotations_list))%len(actions_list))]+[Direction.Drop]
-        #     print("START OF TWO BLOCK SEQUENCE")
+        current_rotation_list = rotations_list[int(index/len(actions_list))]
+        current_action_only_list = [action for action in current_actions_list if action not in current_rotation_list]
+        
+        cloned_board = board.clone()
+        before_number_of_holes = 0
+        for (x,y) in cloned_board.cells:
+            if y==23:
+                continue
+            else:
+                while y<23:
+                    if (x,y+1) in cloned_board.cells:
+                        break
+                    before_number_of_holes+=1
+                    y+=1
+        has_landed = False
+        for rotation in current_rotation_list:
+            if not has_landed:
+                has_landed = cloned_board.rotate(rotation)
+        for action in current_action_only_list:
+            if not has_landed:
+                has_landed = cloned_board.move(action)
+        after_number_of_holes = 0
+        for (x,y) in cloned_board.cells:
+            if y==23:
+                continue
+            else:
+                while y<23:
+                    if (x,y+1) in cloned_board.cells:
+                        break
+                    after_number_of_holes+=1
+                    y+=1
+        sum_of_heights = 0
+        highest_cell_in_each_column = {0:24, 1:24, 2:24, 3: 24, 4: 24, 5: 24, 6: 24, 7: 24, 8: 24, 9: 24}
+        for (x,y) in cloned_board.cells:
+            if highest_cell_in_each_column[x] >= y:
+                highest_cell_in_each_column[x] = y
+        for height in highest_cell_in_each_column.values():
+            sum_of_heights += (24-height)
+        if number_of_discards and not previous_discard:
+            if after_number_of_holes>(before_number_of_holes) and sum_of_heights>60:
+                number_of_discards -= 1
+                previous_discard = True
+                return [Action.Discard]
+        previous_discard = False
         return current_actions_list
 
 
     def choose_action(self, board):
-        global next_actions_list
         # time.sleep(0.5)
-        # # for cell in board.falling.cells:
-        # #     if cell[0] == 1:
-        # #         return Direction.Right
         actions = self.test_action(board)
         for action in actions:
             yield action
         
-
 
 SelectedPlayer = Player
